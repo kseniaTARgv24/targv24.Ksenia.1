2bc8f9d394520f3c83127dc89affabc5dba3f606
kseniaTARgv24 <ksenia.pleshakova2001@gmail.com>
2024-11-20 15:48:09 +0200

    Ül.1

--- targv24.Ksenia.1.py
+++ targv24.Ksenia.1.py
@@ -1,1 +1,10 @@
+#ülesanne 1
 print("bulka")
+nimi = input("Tell me your name, and I'll tell what's your name. Don't believe me? Try and press Enter! ")
+print("Your name is",nimi+"!")
+vanus = int(input("I also can guess your age! so... input your age and press Enter. "))
+print("Your age is",vanus,"haha! See, "+nimi+" I know everything about you! (without f, int only with ,,)")
+print(f"Your age is {vanus} haha! See, {nimi} I know everything about you! (made with f)")
+
+#Ülesanne 2
+
