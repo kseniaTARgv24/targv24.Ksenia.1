ccb8ab34e20291930f9806995476c3edf52fbb8d
kseniaTARgv24 <ksenia.pleshakova2001@gmail.com>
2024-12-04 16:34:32 +0200

    if 2

--- if/if.py
+++ if/if.py
@@ -1,3 +1,6 @@
+
+import random
+
 #from curses.ascii import isalpha 
 
 # print("\n Ul.1\n")
@@ -112,31 +115,65 @@
 #     print("on liiga soe. on vaja 18")
 
 
-print("\n Ul.6\n")
-p=float(input("Ütle kui pikk sa oled cm"))
-if p < 160:
-    print("Oled väike väike")
-    print(r"""
-      /\_/\  
-     ( o.o ) 
-      > ^ <
-     meow""")
-elif p <= 170:
-    print("oled väga ok")
-else:
-    print("olen suur suur")
-    print(r"""
-     /\     /\
-    {  `---'  }
-    {  O   O  }
-    ~~>  V  <~~
-     \  \|/  /
-      `-----'____
-     /     \    \_
-    {       }\  )_\_   _
-     |  \_/  ) / /   \_/ \
-      \__/  /(_/      \__/
-       (__/
-""")
+# print("\n Ul.6\n")
+# p=float(input("Ütle kui pikk sa oled cm"))
+# if p < 160:
+#     print("Oled väike väike")
+#     print(r"""
+#       /\_/\  
+#      ( o.o ) 
+#       > ^ <
+#      meow""")
+# elif p <= 170:
+#     print("oled väga ok")
+# else:
+#     print("olen suur suur")
+#     print(r"""
+#      /\     /\
+#     {  `---'  }
+#     {  O   O  }
+#     ~~>  V  <~~
+#      \  \|/  /
+#       `-----'____
+#      /     \    \_
+#     {       }\  )_\_   _
+#      |  \_/  ) / /   \_/ \
+#       \__/  /(_/      \__/
+#        (__/
+# """)
 
+
+
+print("\n Ul.7\n")
+p=float(input("Ütle kui pikk sa oled cm"))
+s=input("Mis on sinu sugu? (n, m)")
+
+if s.upper()=="N":
+    if p<=155:
+        print("oled lühike")
+    elif p<=175:
+        print("oled ok")
+    else:
+        print("oled pikk")
+elif s.upper()=="M":
+    if p<=165:
+        print("oled lühike")
+    elif p<=185:
+        print("oled ok")
+    else:
+        print("oled pikk") 
+else:
+    print("vasta normaalselt")
+
+
+print("\n Ul.8\n")
+p=input("Kas sa tahad osta piima (jah, ei)")
+l=input("Kas sa tahad osta leiba (jah, ei)")
+m=input("Kas sa tahad osta munade pakki (jah, ei)")
+
+phind=random_float(1.5, 2.5)
+lhind=random_float(1.5, 3.5)
+mhind=random_float(2.5, 5.5)
+
+if p.upper()="JAH":
 
