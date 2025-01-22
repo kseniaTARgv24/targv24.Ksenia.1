f75f1c35939f0c581d8cec1fc08d1ea673ed667c
SeagullToon <SeagullToon@DESKTOP-BIT19CN>
2025-01-22 15:45:55 +0200

    classwork-kasutajafunktsioonid

---
+++ tund 7/classwork-kasutajafunktsioonid.py
@@ -0,0 +1,65 @@
+﻿#~_~
+from module1 import *
+
+# a=int(input("a = "))
+# b=int(input("b = "))
+# c=int(input("c = "))
+# S=summa3(a,b,c)
+# print(S)
+
+# a=float(input("a = "))
+# b=float(input("b = "))
+# c=input("operation = ")
+# S=arithmetic(a,b,c)
+# print(S)
+
+
+
+# while True:
+#     try:
+#         year = int(input("year: "))
+#         break
+#     except:
+#         print("error")
+# S=is_year_leap(year)
+# print(S)
+
+# a=float(input("a = "))
+# V=square(a)
+# print(V)
+
+# a=int(input("month = "))
+# s=season(a)
+# print(s)
+
+# a=float(input("вклад = "))
+# years=int(input("период (года) = "))
+# s=bank(a,years)
+# print(s)
+
+
+b=" "
+while b!="STOP":
+    while True:
+        try:
+            a=float(input("простое ли число? --> "))
+            v=is_prime(a)
+            print(v)
+            break
+        except:
+            print("error")
+    b=input("type 'stop' to exit or enter to continue  ").upper()
+
+
+
+
+
+
+a=int(input("day  "))
+b=int(input("month  "))
+c=int(input("year  "))
+s=date(a,b,c)
+print(s)
+
+
+
---
+++ tund 7/module1.py
@@ -0,0 +1,82 @@
+from re import S
+from xmlrpc.client import boolean
+
+
+def summa3(arv1:int,arv2:int,arv3:int)->int:
+    """
+
+    :param int arv1: Esimene n:
+    :param int arv2: Teine n:
+    :param int arv3: Kolmas n:
+    :rtype: int
+
+    """
+
+    summa = arv1+arv2+arv3
+    return summa
+
+def arithmetic(a:float,b:float,c:str)->float:
+    if c == "+":
+        s=a+b
+    elif c == "-":
+        s=a-b
+    elif c == "*":
+        s=a*b
+    elif c == "/":
+        s=a/b
+    else:
+        s = str("ERROR")
+    return s
+
+
+def is_year_leap(a:int)->bool:
+        if a%4==0:
+            s=True
+        else:
+            s=False
+        return s
+
+def square(a:float)->float:
+    S=a*a
+    P=a*4
+    d=2**(1/2)*a
+    return S,P,d
+
+def season(a:int)->str:
+    if a in [12,1,2]:
+        s="winter"
+    elif a in [3,4,5]:
+        s="spring"
+    elif a in [6,7,8]:
+        s="summer"
+    elif a in [9,10,11]:
+        s="fall"
+    else:
+        s="error"
+    return  s
+
+def bank(a:float,years:int)->float:
+    for i in range (1,years,1):
+        a+=a*0.1
+    return a
+
+def is_prime(a:float)->bool:
+    if a > 1:
+        if a%2 == 0 or a%3==0:
+            s=False
+        else:
+            s=True
+    else:
+        s= False
+    return s
+        
+
+
+
+def date(a:int,b:int,c:int)->bool:
+    if 0<a<32 and 0<b<13 and 0<c<3000:
+        v=True
+    else:
+        v=False
+    return v
+
