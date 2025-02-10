55be791cf168c86e7b73526d02d8056165afcc35
kseniaTARgv24 <ksenia.pleshakova2001@gmail.com>
2025-02-10 19:31:08 +0200

    Praktiline ülesanne "Isikukood" kasutajate funktsioonide abil- VALMIS

---
+++ Functions/IK.py
@@ -0,0 +1,83 @@
+﻿
+
+
+def kontrollnumber(ik)->int:
+    kaal1=[1,2,3,4,5,6,7,8,9,1]
+    kaal2=[3,4,5,6,7,8,9,1,2,3]
+    summ=0
+
+    for i in range(10):
+        mult=int(ik[i])*kaal1[i]
+        summ+=mult
+    if summ%11 == 10:
+        for i in range(10):
+            mult=int(ik[i])*kaal2[i]
+            summ+=mult 
+        if summ%11 == 10:
+            kn = 0
+        else:
+            kn=summ%11
+    else:
+        kn=summ%11
+    return kn
+
+
+def result(ik)->str:
+
+    if ik[0] == "1":
+        v=(ik + " - male," + ik[5:7] + "." + ik[3:5] + ".18" + ik[1:3])
+    elif ik[0] == "2":
+        v=(ik + " - female," + ik[5:7] + "." + ik[3:5] + ".18" + ik[1:3])
+    elif ik[0] == "3":
+        v=(ik + " - male," + ik[5:7] + "." + ik[3:5] + ".19" + ik[1:3])
+    elif ik[0] == "4":
+        v=(ik + " - female," + ik[5:7] + "." + ik[3:5] + ".19" + ik[1:3])
+    elif ik[0] == "5":
+        v=(ik + " - male," + ik[5:7] + "." + ik[3:5] + ".20" + ik[1:3])
+    elif ik[0] == "6":
+        v=(ik + " - female," + ik[5:7] + "." + ik[3:5] + ".20" + ik[1:3])
+
+    return v
+
+def haigla(ik)->str:
+    a = ["Kuressaare Haigla"]
+    b = ["Tartu Ülikooli Naistekliinik", "Tartumaa", "Tartu"]
+    c = ["Ida-Tallinna Keskhaigla", "Pelgulinna sünnitusmaja", "Hiiumaa", "Keila", "Rapla haigla", "Loksa haigla"]
+    d = ["Ida-Viru Keskhaigla", "Kohtla-Järve", "endine Jõhvi"]
+    e = ["Maarjamõisa Kliinikum", "Tartu", "Jõgeva Haigla"]
+    f = ["Narva Haigla"]
+    g = ["Pärnu Haigla"]
+    h = ["Pelgulinna Sünnitusmaja", "Tallinn", "Haapsalu haigla"]
+    i = ["Järvamaa Haigla", "Paide"]
+    j = ["Rakvere", "Tapa haigla"]
+    k = ["Valga Haigla"]
+    l = ["Viljandi Haigla"]
+    m = ["Lõuna-Eesti Haigla", "Võru", "Põlva Haigla"]
+
+    n=int(ik[7:9])
+    if n >= 1 and n <= 10:
+        return a
+    elif n >= 11 and n <= 19:
+        return b
+    elif n >= 21 and n <= 220:
+        return c
+    elif n >= 221 and n <= 270:
+        return d
+    elif n >= 271 and n <= 370:
+        return e
+    elif n >= 371 and n <= 420:
+        return f
+    elif n >= 421 and n <= 470:
+        return g
+    elif n >= 471 and n <= 490:
+        return h
+    elif n >= 491 and n <= 520:
+        return i
+    elif n >= 521 and n <= 570:
+        return j
+    elif n >= 571 and n <= 600:
+        return k
+    elif n >= 601 and n <= 650:
+        return l
+    elif n >= 651 and n <= 700:
+        return m
---
+++ Functions/Praktiline ülesanne Isikukood kasutajate funktsioonide abil.py
@@ -0,0 +1,45 @@
+﻿from IK import *
+
+print("Praktiline ülesanne Isikukood kasutajate funktsioonide abil\n")
+
+ikoodid=[]
+arvud=[]
+stop=" "
+
+while stop != "STOP":
+    ik=input("Enter ikood: ")
+    while len(ik)!=11 or not ik.isdigit():
+        arvud.append(ik)
+        ik=input("Your ikood has wrong number of symbols or contains letters! Enter ikood again (11 symbols and only numbers): ")
+    while ik[0] not in ["1","2","3","4","5","6"]:
+        arvud.append(ik)
+        ik=input("The ikood must start with 1,2,3,4,5 or 6! Enter ik again: ")
+    while True:
+        if 0<=int(ik[1:3])<=99 and 1<=int(ik[3:5])<=12 and 1<=int(ik[5:7])<=31:
+            break
+        else :
+            arvud.append(ik)
+            ik = input("the 2-7 symbols cannot be date of birth. Enter ikood again: ")
+    #Контрольное число
+    # Вес первой степени: 1 2 3 4 5 6 7 8 9 1
+    # Вес второй степени: 3 4 5 6 7 8 9 1 2 3
+
+
+    kn=kontrollnumber(ik)
+
+     
+    if int(ik[10]) == kn:
+        print("ikood is right!(kontrollnumber matches)\n\n")
+        ikoodid.append(ik)
+
+        v=result(ik)
+        v1=haigla(ik)
+        print("\n\n")
+
+    else:
+        print("ikood is wrong... (kontrollnumber doesnt match)")
+        arvud.append(ik)
+ 
+    print("All entered ikoods:\n",ikoodid)
+    print("All wrong attemts:\n",arvud)
+    stop=input("\n\nEnter <<stop>>, if you want to finish. Or press enter to continue--> ").upper()
