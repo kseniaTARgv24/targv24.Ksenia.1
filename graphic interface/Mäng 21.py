e980492de15ca33fb2181f10f32cb02b305a1fc6
kseniaTARgv24 <ksenia.pleshakova2001@gmail.com>
2025-03-11 22:38:30 +0200

    pooleli too (Mang 21)

---
+++ graphic interface/Mäng 21.py
@@ -0,0 +1,90 @@
+﻿from tkinter import *
+from tkinter import messagebox
+from turtle import color
+from PIL import Image, ImageTk
+from numpy import size
+import random
+
+
+root=Tk()
+root.geometry("800x500")
+root.resizable(False,False)
+root.title("21")
+
+
+
+def game():
+    button1.place_forget()
+
+    a = random.randint(2, 11)
+    b = random.randint(2, 11)
+
+    card1.configure(text= str(a))
+    card1.place(x=100, y=100)
+    card2.configure(text= str(b))
+    card2.place(x=500, y=100)
+    summa.configure(text=(f"{a+b}"))
+    summa.place(x=370, y=30)
+    pause.place(x=200, y=400)
+    TakeCard.place(x=500, y=400)
+
+
+
+def pauseChoisedef():
+    card1.place_forget()
+    card2.place_forget()
+    card3.place_forget()
+    ph_label.place(x=0, y=0)
+    Continue.place(x=340, y = 360)
+    showStartMenu()
+
+
+def TakeCardChoisedef():
+    print("di nahui")
+   
+def showStartMenu():
+    Continue.place_forget()
+    ph_label.place_forget()
+    pause.place_forget()
+    TakeCard.place_forget()
+    summa.place_forget()
+
+
+    button1.place(x=340, y = 360)
+
+
+
+original_image = Image.open(r"graphic interface\ttt.png")
+resized_image = original_image.resize((800, 500))  # Resize to window size
+bgimage = ImageTk.PhotoImage(resized_image)
+
+labelBG=Label(root,image=bgimage)
+labelBG.place(x=0,y=0)
+
+button1=Button(root,text="Start",font=("beer money", 45), bg="#36051c", command=game) #command = Start
+button1.place(x=340, y = 360)
+
+cardImage=Image.open(r"graphic interface\images.png")
+bgCardImage=ImageTk.PhotoImage(cardImage)
+
+
+card1=Label(root, image=bgCardImage, text="nr",font=("beer money", 45), compound='center')
+card2=Label(root, image=bgCardImage, text="nr",font=("beer money", 45), compound='center')
+card3=Label(root, image=bgCardImage, text="nr",font=("beer money", 45), compound='center')
+
+ph_label=Label(root, text = "aaaaaaa")
+
+summa=Label(root, bg='pink', text="nr", font=("beer money", 45))
+
+pause=Button(root,text="Pause",font=("beer money", 30), bg="#36051c", command=pauseChoisedef)
+TakeCard=Button(root,text="Take card",font=("beer money", 30), bg="#36051c", command = TakeCardChoisedef)
+Continue=Button(root,text="Continue",font=("beer money", 30), bg="#36051c", command = showStartMenu)
+
+
+
+
+
+
+
+root.mainloop()
+
--- graphic interface/classwork 26.02.py
+++ graphic interface/classwork 26.02.py
@@ -1,88 +1,117 @@
-from msilib.schema import File
+﻿from msilib.schema import File
 from tkinter import *
-from turtle import color
 from tkinter import filedialog
+from tkinter import messagebox
 import smtplib, ssl
-from email.message import *
-import imghdr 
+from email.message import EmailMessage
+import imghdr
 
+files = []
 
+def krysa():
+    for i in range(1, 1000000, 1):
+        message.insert(END, "🐀")
 
 def choose_img():
-    global file
-    file=filedialog.askopenfilename()
-    added.configure(text = file)
+    global files
+    files = filedialog.askopenfilenames(title="Выберите файлы")
+    added.configure(text=str(files))
     added.configure(font=("Freestyle Script", 18))
-    
-    return file
 
 def send():
-    to=email.get()
-    message_box=message.get()
-    Topic=topic.get()
+    toList = email.get().split(", ")
+    message_box = message.get("1.0", END)
+    Topic = topic.get()
 
-
-    smtp_server="smtp.gmail.com"
-    port=587
-    sender_email="ksenia.pleshakova2001@gmail.com"
-    password="pqlx oudg gpzj gumq"
-    context=ssl.create_default_context()
-    msg=EmailMessage()
+    smtp_server = "smtp.gmail.com"
+    port = 587
+    sender_email = "ksenia.pleshakova2001@gmail.com"
+    password = "pqlx oudg gpzj gumq"
+    context = ssl.create_default_context()
+    msg = EmailMessage()
     msg.set_content(message_box)
-    msg['Subject']=Topic
-    msg['From']="aaa"
-    msg['To']=to
+    msg['Subject'] = Topic
+    msg['From'] = sender_email
+    msg['To'] = ", ".join(toList)
 
-    with open(file, 'rb') as fimage:
-        image = fimage.read()
-    msg.add_attachment(image, maintype='image', subtype=imghdr.what(None, image))
+    for file in files:
+        with open(file, 'rb') as fimage:
+            image = fimage.read()
+        msg.add_attachment(image, maintype='image', subtype=imghdr.what(None, image))
+
     try:
-        server=smtplib.SMTP(smtp_server,port)
+        server = smtplib.SMTP(smtp_server, port)
         server.starttls(context=context)
-        server.login(sender_email,password)
+        server.login(sender_email, password)
         server.send_message(msg)
-        message_box.showinfo("info", "sent")
+        messagebox.showinfo("info", "sent")
     except:
-        message_box.showerror("error")
+        messagebox.showerror("error")
     finally:
         server.quit()
 
+def clear():
+    message.delete("1.0", END)
+    files.clear()
+    added.configure(text="...") 
+    email.delete(0, END)
+    topic.delete(0, END)
 
+def add_signature():
+    message.insert(END, "\n\nK.P.\nTARgv24")
 
-root=Tk()
-root.geometry("500x400")
-root.resizable(False,False)
+def preview():
+    To = email.get().strip().split()
+    Topic = topic.get()
+    Message = message.get("1.0", END).strip()
+    preview = f"Email: {''.join(To)}\n\nTopic: {''.join(Topic)}\n\nMessage: {''.join(Message)}\n\nAttached files: {''.join(files)}"
+    messagebox.showinfo("Preview", preview)
+
+root = Tk()
+root.geometry("500x600")
+root.resizable(False, False)
 root.title("Email")
 
+label1 = Label(root, text="EMAIL:", font=("Blackadder ITC", 20))
+label1.place(x=0, y=0)
 
-label1=Label(root, text = "EMAIL:", font=("Blackadder ITC", 20))
-label1.place(x=0,y=0)
+label2 = Label(root, text="TOPIC:", font=("Blackadder ITC", 20))
+label2.place(x=0, y=50)
 
-label2=Label(root, text = "TOPIC:", font=("Blackadder ITC", 20))
-label2.place(x=0,y=50)
+label3 = Label(root, text="ADD:", font=("Blackadder ITC", 20))
+label3.place(x=0, y=100)
 
-label3=Label(root, text = "ADD:", font=("Blackadder ITC", 20))
-label3.place(x=0,y=100)
+label4 = Label(root, text="MESSAGE:", font=("Blackadder ITC", 20))
+label4.place(x=0, y=200)
 
-label4=Label(root, text = "MESSAGE:", font=("Blackadder ITC", 20))
-label4.place(x=0,y=200)
+button1 = Button(root, text="ADD IMAGE", font=("Blackadder ITC", 14), command=choose_img)
+button1.place(x=250, y=350)
 
-button1=Button(root, text = "ADD IMAGE", font=("Blackadder ITC", 14), command=choose_img)
-button1.place(x=250,y=350)
+button2 = Button(root, text="SEND", font=("Blackadder ITC", 14), command=send)
+button2.place(x=400, y=350)
 
-button2=Button(root, text = "SEND", font=("Blackadder ITC", 14), command=send)
-button2.place(x=400,y=350)
+button3 = Button(root, text="CLEAR", font=("Blackadder ITC", 14), command=clear)
+button3.place(x=350, y=450)
 
-email=Entry(root, font=("Freestyle Script", 25))
-email.place(x=190, y =0, width= 300, height=50)
+button4 = Button(root, text="SIGNATURE", font=("Blackadder ITC", 14), command=add_signature)
+button4.place(x=150, y=450)
 
-topic=Entry(root, font=("Freestyle Script", 25))
-topic.place(x=190, y =50, width= 300, height=50)
+button4 = Button(root, text="PREVIEW", font=("Blackadder ITC", 14), command=preview)
+button4.place(x=50, y=450)
 
-added=Label(root, text="...", font=("Freestyle Script", 25))
-added.place(x=190, y =110, width= 300, height=20)
+button5 = Button(root, text="🐀", font=("Blackadder ITC", 14), command=krysa)
+button5.place(x=50, y=550)
 
-message=Entry(root, font=("Freestyle Script", 25))
-message.place(x=190, y =130, width= 300, height=200)
+email = Entry(root, font=("Freestyle Script", 25))
+email.place(x=190, y=0, width=300, height=50)
+
+topic = Entry(root, font=("Freestyle Script", 25))
+topic.place(x=190, y=50, width=300, height=50)
+
+added = Label(root, text="...", font=("Freestyle Script", 25))
+added.place(x=190, y=110, width=300, height=20)
+
+message = Text(root, font=("Freestyle Script", 25))
+message.place(x=190, y=130, width=300, height=200)
 
 root.mainloop()
